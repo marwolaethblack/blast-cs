d98c13422269467aed7b40f8410196bdea5df957
Adnan Unal <a.unal677@gmail.com>
2026-01-19 14:49:20 +0100

    feat: display player names

--- app/app/components/player/PlayerDot.tsx
+++ app/app/components/player/PlayerDot.tsx
@@ -1,40 +1,49 @@
 import { FunctionComponent } from "react";
+import { CSTeam } from "../../../../api/events/types";
+import { teamBackgroundColors, teamDeadColors } from "@/app/utils/colors";
 
 interface IProps {
-  color: string;
   left: number;
   top: number;
   dead: boolean;
+  team: CSTeam;
+  name: string;
 }
 
 export const PlayerDot: FunctionComponent<IProps> = ({
-  color,
+  team,
   left,
   top,
   dead,
+  name,
 }) => {
   if (dead) {
     return (
-      <svg
-        width="8px"
-        height="8px"
-        viewBox="0 0 512 512"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        aria-hidden="true"
-        role="img"
-        className="iconify iconify--fxemoji z-20 absolute fill-red-400"
-        preserveAspectRatio="xMidYMid meet"
-        style={{ left, top }}
-      >
-        <path d="M330.443 256l136.765-136.765c14.058-14.058 14.058-36.85 0-50.908l-23.535-23.535c-14.058-14.058-36.85-14.058-50.908 0L256 181.557L119.235 44.792c-14.058-14.058-36.85-14.058-50.908 0L44.792 68.327c-14.058 14.058-14.058 36.85 0 50.908L181.557 256L44.792 392.765c-14.058 14.058-14.058 36.85 0 50.908l23.535 23.535c14.058 14.058 36.85 14.058 50.908 0L256 330.443l136.765 136.765c14.058 14.058 36.85 14.058 50.908 0l23.535-23.535c14.058-14.058 14.058-36.85 0-50.908L330.443 256z"></path>
-      </svg>
+      <div className="z-20 absolute " style={{ left, top }}>
+        <svg
+          width="8px"
+          height="8px"
+          viewBox="0 0 512 512"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          aria-hidden="true"
+          role="img"
+          className=" bg-blend-lighten"
+          preserveAspectRatio="xMidYMid meet"
+          style={{ fill: teamDeadColors[team] }}
+        >
+          <path d="M330.443 256l136.765-136.765c14.058-14.058 14.058-36.85 0-50.908l-23.535-23.535c-14.058-14.058-36.85-14.058-50.908 0L256 181.557L119.235 44.792c-14.058-14.058-36.85-14.058-50.908 0L44.792 68.327c-14.058 14.058-14.058 36.85 0 50.908L181.557 256L44.792 392.765c-14.058 14.058-14.058 36.85 0 50.908l23.535 23.535c14.058 14.058 36.85 14.058 50.908 0L256 330.443l136.765 136.765c14.058 14.058 36.85 14.058 50.908 0l23.535-23.535c14.058-14.058 14.058-36.85 0-50.908L330.443 256z"></path>
+        </svg>
+        <div className="text-xs text-white">{name}</div>
+      </div>
     );
   }
   return (
-    <div
-      className={`${dead ? "bg-amber-950!" : "rounded-full"} w-2 h-2 absolute z-21 ${color} border border-solid border-blue-200`}
-      style={{ left, top }}
-    />
+    <div style={{ left, top }} className="absolute z-21">
+      <div
+        className={`${dead ? "bg-amber-950!" : "rounded-full"} w-2 h-2  ${teamBackgroundColors[team]} border border-solid border-blue-200`}
+      />
+      <div className="text-xs text-white">{name}</div>
+    </div>
   );
 };
