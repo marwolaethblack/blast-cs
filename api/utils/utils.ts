1875f0c75850413e75fb8c78c89c7d2974f04c7d
Adnan Unal <a.unal677@gmail.com>
2026-01-20 10:29:20 +0100

    fix: parse damage numbers as int

--- api/utils/utils.ts
+++ api/utils/utils.ts
@@ -1,12 +1,4 @@
 export const parseCoordinateString = (coord: string) => {
-  console.log(coord);
-  console.log(
-    coord
-      .replace("[", "")
-      .replace("]", "")
-      .split(" ")
-      .map((v) => parseInt(v)),
-  );
   return coord
     .replace("[", "")
     .replace("]", "")
